551429ec4cc1c2e1ce86499be7cb375f7a98fc44
Gno <genoyoyo@gmail.com>
2017-08-15 08:23:08 +0700

    initial commit

---
+++ app/src/main/java/com/tnis/tngp17_001/tapper/MainActivity.java
@@ -0,0 +1,16 @@
+package com.tnis.tngp17_001.tapper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText editText1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
