45d800d67913aa62e10cd3adb0d01efcd50e8c12
Gno <genoyoyo@gmail.com>
2017-08-15 13:25:52 +0700

    Change game logic

--- app/src/main/java/com/tnis/tngp17_001/tapper/MainActivity.java
+++ app/src/main/java/com/tnis/tngp17_001/tapper/MainActivity.java
@@ -1,16 +1,48 @@
 package com.tnis.tngp17_001.tapper;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText editText1;
+    private ImageButton btnPlay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText1 = (EditText) findViewById(R.id.editText1);
+        btnPlay = (ImageButton) findViewById(R.id.btnPlay);
+
+
+
+
+        btnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int val=0;
+
+                try {
+                    val = Integer.parseInt(editText1.getText().toString());
+                } catch (NumberFormatException e){
+                }
+
+
+                Intent intent = new Intent(MainActivity.this,SecondActivity.class);
+                intent.putExtra("result",val);
+
+                startActivity(intent);
+
+
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/tnis/tngp17_001/tapper/SecondActivity.java
+++ app/src/main/java/com/tnis/tngp17_001/tapper/SecondActivity.java
@@ -1,13 +1,52 @@
 package com.tnis.tngp17_001.tapper;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
+
+    int val = 0;
+    int count = 0;
+    private ImageButton btnCoin;
+    private TextView   tvNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+
+        tvNumber = (TextView) findViewById(R.id.tvNumber);
+        btnCoin = (ImageButton) findViewById(R.id.btnCoin);
+
+
+
+
+        Intent intent = getIntent();
+        val = intent.getIntExtra("result",0);
+
+        tvNumber.setText(count+"");
+
+        btnCoin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                if (count >= val-1){
+                    Intent intent = new Intent(SecondActivity.this,MainActivity.class);
+                    startActivity(intent);
+                }
+
+                if (val != 0)
+                count += 1;
+                tvNumber.setText(count+"");
+
+            }
+        });
+
     }
 }
